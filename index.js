a2dc64f6de0d25074064dea85d873f01a64ba8ab
rafinsripon <rafinsripon@gmail.com>
2022-12-26 13:35:25 +0600

    project demo start

---
+++ index.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const cors = require("cors");
+const jwt = require("jsonwebtoken");
+const app = express();
+const port = process.env.PORT || 5000;
+require("dotenv").config();
+
+
+// middlewares
+app.use(cors());
+app.use(express.json());
+
+async function run(){
+    try{
+        console.log("Database Connected");
+    }finally{
+
+    }
+}
+run().catch(error => console.log(error));
+
+
+
+//default setup
+app.get("/", (req, res) => {
+    res.send("Bookely - server is running on port")
+})
+
+app.listen(port, () => {
+    console.log(`server is runnig on ${port}`)
+})
